b4ad12ab558b02d1d022b34c5a9ef802358004a0
Veronica Trias <vero.ramm@gmail.com>
2023-01-06 09:58:35 -0300

    Modifique carpetas

---
+++ src/jdbc/factory/TesteConexion.java
@@ -0,0 +1,15 @@
+package jdbc.factory;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TesteConexion {
+	public static void main(String[] args) throws SQLException {
+
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexion();
+
+		System.out.println("Cerrando conexion!!");
+
+		connection.close();
+	}
+}
